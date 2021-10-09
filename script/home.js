c6bbc49a56243379c2670da22eb207ea977f6bca
trong19130249 <19130249@st.hcmuaf.edu.vn>
2021-10-09 09:06:36 +0700

    feat: header scroll

--- script/home.js
+++ script/home.js
@@ -1,7 +1,5 @@
 //  slide show
 var slideIndex = 1;
-showDivs(slideIndex);
-
 function plusDivs(n) {
   showDivs((slideIndex += n));
 }
@@ -9,15 +7,17 @@ function plusDivs(n) {
 function currentDiv(n) {
   showDivs((slideIndex = n));
 }
+showDivs(slideIndex);
 
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   var slideShowa = document.getElementById("ml-slide");
-  if (n > x.length) {
+  if (n > x.length || slideIndex > x.length) {
     slideIndex = 1;
   }
+
   if (n < 1) {
     slideIndex = x.length;
   }
@@ -29,5 +29,27 @@ function showDivs(n) {
 
   if (dots[slideIndex - 1]) {
     dots[slideIndex - 1].className += " active";
+  }
+}
+window.onload = function (e) {
+  setInterval(() => {
+    showDivs(slideIndex++);
+  }, 10000);
+};
+
+//===============================header scroll ======================
+window.onscroll = function () {
+  scrollHideNav();
+};
+var header = document.getElementById("header-scroll");
+
+function scrollHideNav() {
+  var doc = document.documentElement;
+  var w = window;
+  var prevScroll = w.pageYOffset || w.scrollY || doc.scrollTop;
+  if (window.pageYOffset > 20) {
+    header.classList.add("header-scroll-active");
+  } else {
+    header.classList.remove("header-scroll-active");
   }
 }
