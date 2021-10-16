ff9f3f4ea250eda638ca0dbc38f9d010bd05b4fd
trong19130249 <19130249@st.hcmuaf.edu.vn>
2021-10-17 00:25:23 +0700

    Feat:slider section(v1)

--- script/home.js
+++ script/home.js
@@ -35,6 +35,9 @@ window.onload = function (e) {
   setInterval(() => {
     showDivs(slideIndex++);
   }, 10000);
+  //   setInterval(() => {
+  //     showSlide(++indexSlider);
+  //   }, 6000);
 };
 
 //==========================hover image dom===========================//
@@ -42,8 +45,8 @@ const listImageHover = document.querySelectorAll(".img-item-hov");
 listImageHover.forEach((element) => {
   element.addEventListener("mouseover", () => {
     element
-      .closest(".slide-colection")
-      .querySelector(".colection-slide-image").src = element.src;
+      .closest(".slide-collection")
+      .querySelector(".collection-slide-image").src = element.src;
   });
 });
 
@@ -51,7 +54,7 @@ listImageHover.forEach((element) => {
 const listNextBtn = document.querySelectorAll(".next-btn");
 const listPrevBtn = document.querySelectorAll(".prev-btn");
 const listSlideshowCollection = document.querySelectorAll(
-  ".slideshow-colection"
+  ".slideshow-collection"
 );
 listSlideshowCollection.forEach((element) => {
   element.style.transform = "translateX(0%)";
@@ -60,9 +63,9 @@ listSlideshowCollection.forEach((element) => {
 listNextBtn.forEach((element) => {
   element.addEventListener("click", () => {
     let parentE = element
-      .closest(".colection-slide")
-      .querySelector(".slideshow-colection");
-    let count = parentE.querySelectorAll(".slide-colection").length;
+      .closest(".collection-slide")
+      .querySelector(".slideshow-collection");
+    let count = parentE.querySelectorAll(".slide-collection").length;
     let percentBefore = parentE.style.transform.replace(/[^-?\d.]/g, "");
     percentBefore = parseInt(percentBefore, 10) - 50;
     if (percentBefore < -(count - 2) * 50) {
@@ -74,15 +77,43 @@ listNextBtn.forEach((element) => {
 listPrevBtn.forEach((element) => {
   element.addEventListener("click", () => {
     let parentE = element
-      .closest(".colection-slide")
-      .querySelector(".slideshow-colection");
-    let count = parentE.querySelectorAll(".slide-colection").length;
+      .closest(".collection-slide")
+      .querySelector(".slideshow-collection");
+    let count = parentE.querySelectorAll(".slide-collection").length;
     let percentBefore = parentE.style.transform.replace(/[^-?\d.]/g, "");
     percentBefore = parseInt(percentBefore, 10) + 50;
+    console.log(percentBefore);
     if (percentBefore > 0) {
       percentBefore = -(count - 2) * 50;
-      console.log(percentBefore);
     }
     parentE.style.transform = `translateX(${percentBefore}%)`;
   });
 });
+//==========================section-slider===========================//
+const slickNext = document.getElementById("slick-next");
+const slickPrev = document.getElementById("slick-prev");
+const sectionContent = document.getElementById("section-slideshow");
+const listSlider = document.querySelectorAll(".slides-item");
+
+var indexSlider = 1;
+slickNext.addEventListener("click", () => {
+  showSlide(++indexSlider);
+});
+slickPrev.addEventListener("click", () => {
+  showSlide(--indexSlider);
+});
+showSlide(1);
+function showSlide(n) {
+  if (indexSlider > listSlider.length - 2) {
+    indexSlider = 1;
+  }
+  if (n < 1) {
+    indexSlider = listSlider.length - 2;
+  }
+  listSlider.forEach((element) => {
+    element.className = "slides-item";
+  });
+  let percent = -(indexSlider - 1) * 33.33;
+  sectionContent.style.transform = `translateX(${percent}%)`;
+  listSlider[indexSlider].className = "slides-item slide-active";
+}
