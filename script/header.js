7c3cadffddef5b9cdc8cb3a6c9140caa961b37b4
trong19130249 <19130249@st.hcmuaf.edu.vn>
2021-12-02 20:25:01 +0700

    feat: search

--- script/header.js
+++ script/header.js
@@ -14,3 +14,13 @@ function scrollHideNav() {
     header.classList.remove("header-scroll-active");
   }
 }
+//===============================Scroll-top ======================
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  //if scrollUp not null
+  if (scrollUp) {
+    if (this.scrollY >= 500) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
+  }
+}
+window.addEventListener("scroll", scrollUp);
